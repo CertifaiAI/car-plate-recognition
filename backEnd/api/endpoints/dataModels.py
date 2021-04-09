00a679754daaea10eb8ce5f0c7ca866b9f0b7411
Nexlson <nelsonchoon112@gmail.com>
2021-04-09 09:13:47 +0800

    update struc

---
+++ backEnd/api/endpoints/dataModels.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+class Users(BaseModel):
+    carplate_no: str
+    name: str
+    emp_id: str
+
+class carImage(BaseModel):
+    image: str
+
+class plateImage(BaseModel):
+    image: str
--- backEnd/api/endpoints/models/users.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class Users(BaseModel):
-    carplate_no: str
-    name: str
-    emp_id: str
