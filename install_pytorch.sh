4e16e52a85859608ffda72427b8ef2415b129e81
Abu <68045710+abuelgasimsaadeldin@users.noreply.github.com>
2021-05-19 15:49:42 +0800

    update pytorch installation

--- install_pytorch.sh
+++ install_pytorch.sh
@@ -12,13 +12,13 @@ sudo apt-get install python3-setuptools
 #For latest PyTorch refer original Nvidia Jetson Nano thread - https://devtalk.nvidia.com/default/topic/1049071/jetson-nano/pytorch-for-jetson-nano/.
 #Choose the version for pytorch and torchvision by changing version
 wget https://nvidia.box.com/shared/static/9eptse6jyly1ggt9axbja2yrmj6pbarc.whl -O torch-1.6.0-cp36-cp36m-linux_aarch64.whl
-sudo apt-get install python3-pip libopenblas-base
+sudo apt-get install python3-pip libopenblas-base libopenmpi-dev 
 sudo pip3 install Cython
 sudo pip3 install torch-1.6.0-cp36-cp36m-linux_aarch64.whl
 
 #Installing torchvision
 #For latest torchvision refer original Nvidia Jetson Nano thread - https://devtalk.nvidia.com/default/topic/1049071/jetson-nano/pytorch-for-jetson-nano/.
-sudo apt-get install libjpeg-dev zlib1g-dev
+sudo apt-get install libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
 git clone --branch release/0.7 https://github.com/pytorch/vision torchvision   # see below for version of torchvision to download
 cd torchvision
 sudo python3 setup.py install
