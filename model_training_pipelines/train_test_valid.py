b4c5e848a436117f8f3b39681dddf54d1c510577
Abu <68045710+abuelgasimsaadeldin@users.noreply.github.com>
2021-06-13 01:58:11 +0800

    update train_test_valid.py

--- model_training_pipelines/train_test_valid.py
+++ model_training_pipelines/train_test_valid.py
@@ -11,11 +11,11 @@ import shutil
 arg = argparse.ArgumentParser()
 arg.add_argument('--dir', help='Path to dataset', required=True, type=str)
 arg.add_argument('--train', help='Ratio for train dataset', default=0.7, type=float)
-arg.add_argument('--test', help='Ratio for test dataset', default=0.2, type=float)
-arg.add_argument('--valid', help='Ratio for valid dataset', default=0.1, type=float)
-arg.add_argument('--train_out', help='Train dataset output path', required=True, type=str)
-arg.add_argument('--test_out', help='Test dataset output path', required=True, type=str)
-arg.add_argument('--valid_out', help='Valid dataset output path', required=True, type=str)
+arg.add_argument('--test', help='Ratio for test dataset', default=0.1, type=float)
+arg.add_argument('--valid', help='Ratio for valid dataset', default=0.2, type=float)
+arg.add_argument('--train_out', help='Train dataset output path', type=str)
+arg.add_argument('--test_out', help='Test dataset output path', type=str)
+arg.add_argument('--valid_out', help='Valid dataset output path', type=str)
 # arg.add_argument('--annotation', help='Annotation file type', required=True, type=str)
 cfg = arg.parse_args()
 
@@ -35,8 +35,8 @@ def split(train_ratio, test_ratio, valid_ratio, image_files):
     test_num = int(data_count * test_ratio)
     valid_num = int(data_count - train_num - test_num)
     print("Training dataset will have {} images".format(train_num))
-    print("Test dataset will have {} images".format(test_num))
     print("Valid dataset will have {} images".format(valid_num))
+    print("Test dataset will have {} images".format(test_num))
     # Get train data
     train_data = image_files[:train_num]
     image_files = image_files[train_num:]
@@ -94,18 +94,24 @@ def main():
     # Check correct ratio
     check_ratio(cfg.train, cfg.test, cfg.valid)
     # Check entered path exists or not
-    check_dir_exist(cfg.train_out)
-    check_dir_exist(cfg.test_out)
-    check_dir_exist(cfg.valid_out)
+    # check_dir_exist(cfg.train_out)
+    # check_dir_exist(cfg.test_out)
+    # check_dir_exist(cfg.valid_out)
     # Get files from directory
     images, annotations, counts = get_file_lists(cfg.dir)
     print('Total number of images detected: {}'.format(len(images)))
     # Split files
     train_data, test_data, valid_data = split(cfg.train, cfg.test, cfg.valid, images)
     # Copy files into train test valid folder
-    move_files(cfg.dir, train_data, cfg.train_out, 'train')
-    move_files(cfg.dir, test_data, cfg.test_out, 'test')
-    move_files(cfg.dir, valid_data, cfg.valid_out, 'valid')
+    # move_files(cfg.dir, train_data, cfg.train_out, 'train')
+    # move_files(cfg.dir, test_data, cfg.test_out, 'test')
+    # move_files(cfg.dir, valid_data, cfg.valid_out, 'valid')
+    os.makedirs(cfg.dir+'/train')
+    os.makedirs(cfg.dir+'/test/')
+    os.makedirs(cfg.dir+'/valid')
+    move_files(cfg.dir, train_data, cfg.dir+'/train', 'train')
+    move_files(cfg.dir, test_data, cfg.dir+'/test', 'test')
+    move_files(cfg.dir, valid_data, cfg.dir+'/valid', 'valid')
     # Zip the folder
 
 if __name__ == '__main__':
