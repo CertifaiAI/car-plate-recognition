fc96fdcb176c436609e24bff3a30c363997fde76
Abu <ashaikh.saad.as@gmail.com>
2021-07-30 09:32:17 +0800

    Remove Shell Scripts

--- Carplate-yolov5/install_scripts.sh
+++
@@ -1,8 +0,0 @@
-# Install torch
-bash install_torch.sh
-
-# Install third party libraries
-pip install -r requirements.txt
-
-# Download weights
-gdown https://drive.google.com/uc?id=18tyNWkGC_x9FddZ9hJ5di3_Sc9WPkpd_ -O yolov5/weights/detection.pt
--- Carplate-yolov5/install_torch.sh
+++
@@ -1,1 +0,0 @@
-pip3 install torch==1.9.0+cpu torchvision==0.10.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
