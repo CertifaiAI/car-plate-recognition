f64c4fbd3325759afa00f9ac613c8bc7f3326954
Nexlson <nelsonchoon112@gmail.com>
2021-08-02 10:53:04 +0800

    update scripts

--- install_torch.sh
+++ install_torch.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 # install pytorch 1.8
 wget https://nvidia.box.com/shared/static/p57jwntv436lfrd78inwl7iml6p13fzh.whl -O torch-1.8.0-cp36-cp36m-linux_aarch64.whl
 sudo apt-get install python3-pip libopenblas-base libopenmpi-dev 
