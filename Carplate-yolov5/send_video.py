d0a49f738f4ea6f2e95def77535ead5227090d55
abuelgasimsaadeldin <ashaikh.saad.as@gmail.com>
2021-08-06 16:26:18 +0800

    Update send_video.py

--- Carplate-yolov5/send_video.py
+++ Carplate-yolov5/send_video.py
@@ -7,8 +7,8 @@ import torch
 import torch.backends.cudnn as cudnn
 
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, \
+from utils.datasets import LoadStreams
+from utils.general import check_img_size, check_requirements, non_max_suppression, \
     scale_coords, set_logging, save_one_box
 from utils.plots import colors, plot_one_box
 from utils.torch_utils import select_device, time_synchronized
@@ -20,46 +20,27 @@ import json
 from PIL import Image
 import io
 
-all_access = ['PFQ5217', 'PFQ 5217']
+# all_access = ['PFQ5217', 'PFQ 5217']
 
 
 @torch.no_grad()
-def detect(weights='weights/detection.pt',  # model.pt path(s)
-           source='data/images',  # file/dir/URL/glob, 0 for webcam
-           imgsz=416,  # inference size (pixels)
-           conf_thres=0.6,  # confidence threshold
-           iou_thres=0.45,  # NMS IOU threshold
-           device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-           view_img=True,  # show results || Modified
-           save_crop=True,  # save cropped prediction boxes || Modified
-           ):
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+def detect(show, nano, imgsz=416, device=''):
 
     #gate = GateControl()
 
     # Initialize
-    set_logging()
+    set_logging()  # just for logging
     device = select_device(device)
-    device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
-    model = attempt_load(weights, map_location=device)  # load FP32 model
+    model = attempt_load(weights='weights/detection.pt', map_location=device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
-    imgsz = check_img_size(imgsz, s=stride)  # check image size
+    imgsz = check_img_size(imgsz, s=stride)  # check image size (if accidentally put 400 will round to nearest)
     names = model.module.names if hasattr(model, 'module') else model.names  # get class names
 
     # Set Dataloader
-    vid_path, vid_writer = None, None
-    if webcam:
-        view_img = check_imshow()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-    else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride)
-
-    # If you would like to specify unique Car, Van and NumberPlate Bounding box colours
-    # colors = {0: (51, 51, 255), 1: (102, 0, 204), 2: (0, 128, 255)}
+    cudnn.benchmark = True  # set True to speed up constant image size inference
+    dataset = LoadStreams(sources='0', img_size=imgsz, stride=stride, nano=nano)
 
     # Run inference
     if device.type != 'cpu':
@@ -77,20 +58,17 @@ def detect(weights='weights/detection.pt',  # model.pt path(s)
         pred = model(img)[0]
 
         # Apply NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, max_det=1000)
+        pred = non_max_suppression(pred, conf_thres=0.6, iou_thres=0.45, max_det=1000)
         t2 = time_synchronized()
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
-            else:
-                p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+            p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
 
             p = Path(p)  # to Path
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            imc = im0.copy() if save_crop else im0  # for save_crop
+            imc = im0.copy()  # saved cropped image
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
@@ -102,37 +80,37 @@ def detect(weights='weights/detection.pt',  # model.pt path(s)
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
-                    if save_crop or view_img:  # Add bbox to image
+                    if show:  # Add bbox to image
                         c = int(cls)  # integer class
                         label = f'{names[c]} {conf:.2f}'
                         plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=3)
-                        #plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3) # unique colors
-                        if save_crop:
-                            # If remainder of the current frame divided by 25 is 0 and label is NP run the function
-                            if frame % 20 == 0 and names[c] == 'NumberPlate':
-                                def cv2Img_base64Img(cv2Img):
-                                    # array to Pil
-                                    image = Image.fromarray(cv2Img)
-                                    # Pil to bytes
-                                    buffer = io.BytesIO()
-                                    image.save(buffer, format="JPEG")
-                                    return base64.b64encode(buffer.getvalue()).decode('ascii')
 
-                                carImage = save_one_box(xyxy, imc, file=f'{p.stem}.jpg', BGR=True)
-                                carImage = cv2.cvtColor(carImage, cv2.COLOR_BGR2RGB)
-                                carImage = cv2Img_base64Img(carImage)
-                                data = {"image": carImage}
-                                response = requests.post('http://localhost:8000/api/ocr/ocr', data=json.dumps(data))
-                                print(response.text)
-                                #if json.loads(response.text)['Plate Number'] in all_access:
-                                    #print("Vehicle is authorized")
-                                    #gate.relay_on()
+                        # Conditions of when to send cropped images to the server
+                        # If remainder of the current frame divided by 25 is 0 and label is NP run the function
+                        if frame % 20 == 0 and names[c] == 'NumberPlate':
+                            def cv2Img_base64Img(cv2Img):
+                                # array to Pil
+                                image = Image.fromarray(cv2Img)
+                                # Pil to bytes
+                                buffer = io.BytesIO()
+                                image.save(buffer, format="JPEG")
+                                return base64.b64encode(buffer.getvalue()).decode('ascii')
+
+                            carImage = save_one_box(xyxy, imc, file=f'{p.stem}.jpg', BGR=True)
+                            carImage = cv2.cvtColor(carImage, cv2.COLOR_BGR2RGB)
+                            carImage = cv2Img_base64Img(carImage)
+                            data = {"image": carImage}
+                            response = requests.post('http://localhost:8000/api/ocr/ocr', data=json.dumps(data))
+                            print(response.text)
+                            # if json.loads(response.text)['Plate Number'] in all_access:
+                            #     print("Vehicle is authorized")
+                            #     gate.relay_on()
 
             # Print time (inference + NMS)
-            print(f'{s}Done. ({t2 - t1:.3f}s)') # Example print out: "256x416 1 Car, 1 NumberPlate, Done. (0.016s)"
+            print(f'{s}Done. ({t2 - t1:.3f}s)')  # Example print out: "256x416 1 Car, 1 NumberPlate, Done. (0.016s)"
 
             # Stream results
-            if view_img:
+            if show:
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
 
@@ -141,15 +119,12 @@ def detect(weights='weights/detection.pt',  # model.pt path(s)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='weights/detection.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/videos/carplate2.mp4', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=416, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.6, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--show', action='store_true', default=False, help='show results')
+    parser.add_argument('--nano', action='store_true', default=False, help='use nano')
+    parser.add_argument('--imgsz', type=int, default=416, help='inference size (pixels)')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', default=True, help='show results')
-    parser.add_argument('--save-crop', action='store_true', default=True, help='save cropped prediction boxes')
     opt = parser.parse_args()
     print(opt)
+
     check_requirements()
     detect(**vars(opt))
