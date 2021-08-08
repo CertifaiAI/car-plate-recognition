c3d9b14682cc34f40130b490b0333da4f1477110
abuelgasimsaadeldin <ashaikh.saad.as@gmail.com>
2021-08-08 23:23:57 +0800

    minimal script

--- Carplate-yolov5/send_video.py
+++ Carplate-yolov5/send_video.py
@@ -11,14 +11,12 @@ from yolov5.utils.datasets import LoadStreams
 from yolov5.utils.general import check_img_size, non_max_suppression, scale_coords, save_one_box
 from yolov5.utils.plots import colors, plot_one_box
 from yolov5.utils.torch_utils import select_device, time_synchronized
-
-#from utils.gate_control import GateControl
+from functions import cv2Img_base64Img
 
 import requests
-import base64
 import json
-from PIL import Image
-import io
+
+#from utils.gate_control import GateControl
 
 # all_access = ['PFQ5217', 'PFQ 5217']
 
@@ -26,7 +24,7 @@ import io
 @torch.no_grad()
 def detect(show, nano, imgsz=416, device=''):
 
-    #gate = GateControl()
+    # gate = GateControl()
 
     # Initialize
     device = select_device(device)
@@ -86,24 +84,24 @@ def detect(show, nano, imgsz=416, device=''):
 
                         # Conditions of when to send cropped images to the server
                         # If remainder of the current frame divided by 25 is 0 and label is NP run the function
-                        if frame % 20 == 0 and names[c] == 'NumberPlate':
-                            def cv2Img_base64Img(cv2Img):
-                                # array to Pil
-                                image = Image.fromarray(cv2Img)
-                                # Pil to bytes
-                                buffer = io.BytesIO()
-                                image.save(buffer, format="JPEG")
-                                return base64.b64encode(buffer.getvalue()).decode('ascii')
 
-                            carImage = save_one_box(xyxy, imc, file=f'{p.stem}.jpg', BGR=True)
-                            carImage = cv2.cvtColor(carImage, cv2.COLOR_BGR2RGB)
-                            carImage = cv2Img_base64Img(carImage)
-                            data = {"image": carImage}
-                            response = requests.post('http://localhost:8000/api/ocr/ocr', data=json.dumps(data))
-                            print(response.text)
-                            # if json.loads(response.text)['Plate Number'] in all_access:
-                            #     print("Vehicle is authorized")
-                            #     gate.relay_on()
+                        if frame % 20 == 0 and names[c] == 'NumberPlate':
+                            # Crop plate image
+                            plate_image = save_one_box(xyxy, imc, file=f'{p.stem}.jpg', BGR=True)
+
+                            # Convert cv2 image to base64 str
+                            plate_image = cv2.cvtColor(plate_image, cv2.COLOR_BGR2RGB)
+                            plate_image_base64 = cv2Img_base64Img(plate_image)
+
+                            try:
+                                data = {"image": plate_image_base64}
+                                response = requests.post('http://localhost:8000/api/ocr/ocr', data=json.dumps(data))
+                                print(response.text)
+                                # if json.loads(response.text)['Plate Number'] in all_access:
+                                #     print("Vehicle is authorized")
+                                #     gate.relay_on()
+                            except:
+                                print("Failed to send plate to server")
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({t2 - t1:.3f}s)')  # Example print out: "256x416 1 Car, 1 NumberPlate, Done. (0.016s)"
