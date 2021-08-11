03149ed2cd51eb2e07bb2527279bd8f208389bf9
Nexlson <nelsonchoon112@gmail.com>
2021-08-11 13:03:56 +0800

    added led panel code

--- Carplate-yolov5/ledPanel.py
+++ Carplate-yolov5/ledPanel.py
@@ -2,9 +2,16 @@
     Send data to bluetooth device to display data on P10 LED Panel
 '''
 
+import serial
+
 class LedPanel():
     def __init__(self):
-        pass
+        # Set USB port for arduino
+        self.ser= serial.Serial('/dev/ttyACM0', 9600)
+        self.welcome_message = 'Welcome to Skymind'
 
     def send_data(self, data):
-        pass
+        # Send to LED
+        self.ser.write(data.encode())
+        # Write back to ori
+        self.ser.write(self.welcome_message.encode())
