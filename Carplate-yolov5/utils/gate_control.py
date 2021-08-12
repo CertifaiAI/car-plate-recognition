9eca21bb553e895f4f58de54655ba72e9d76dc7d
Nexlson <nelsonchoon112@gmail.com>
2021-08-12 13:00:20 +0800

    edit mqtt status report

--- Carplate-yolov5/utils/gate_control.py
+++ Carplate-yolov5/utils/gate_control.py
@@ -9,13 +9,17 @@ class GateControl:
 	def __init__(self):
 		self.output_pin = 35  # gpio76
 		self.delay = 2
+		# gate off
+		self.gate_status = 0
 		GPIO.setup(self.output_pin, GPIO.OUT, initial=GPIO.LOW)
 
 	def relay_on(self):
 		print('Relay ON')
 		GPIO.output(self.output_pin, GPIO.HIGH)  # Turn relay on
+		self.gate_status = 1 # gate on
 		time.sleep(self.delay)
 		print('Relay OFF')
 		GPIO.output(self.output_pin, GPIO.LOW)  # Turn relay off
+		self.gate_status = 0 # gate off
 		time.sleep(self.delay)
 		# GPIO.cleanup()
