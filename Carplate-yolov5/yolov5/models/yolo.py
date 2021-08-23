b37c3cb515a2300e00ceb700857b029d180f9842
Nexlson <nelsonchoon112@gmail.com>
2021-08-23 23:54:24 +0800

    update missing yolo files

--- Carplate-yolov5/yolov5/models/yolo.py
+++ Carplate-yolov5/yolov5/models/yolo.py
@@ -18,7 +18,7 @@ from models.experimental import *
 from yolov5.utils.autoanchor import check_anchor_order
 # from utils.general import make_divisible, check_file, set_logging
 from yolov5.utils.general import make_divisible
-from utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
+from yolov5.utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
     select_device, copy_attr
 
 try:
