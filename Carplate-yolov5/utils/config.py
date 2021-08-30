220c73f43c5c3f2a10c71a89d01e6539efaa15b1
Nexlson <nelsonchoon112@gmail.com>
2021-08-30 09:24:21 +0800

    add comments on config

--- Carplate-yolov5/utils/config.py
+++ Carplate-yolov5/utils/config.py
@@ -4,30 +4,22 @@
 
 class Config: 
     def __init__(self):
-        # Server
-        # self.SERVER_URL = 'http://139.162.33.89:8080/api/v1/r69C44coGlPTANdNBnqf/rpc'
+        # URL for kraftboard server
         self.SERVER_URL = 'http://139.162.33.89:8080/api/v1/cCTjoonMOneHUkz4iDdI/rpc'
 
-
-        # Model
+        # Model path 
         self.WEIGHTS_PATH = 'yolov5/weights/detection.pt'
+        # Model settings 
         self.DEVICE = 'cpu'
 
-        # Sensor
+        # Sensor distance (object to device)
         self.SENSOR_DIST = 300
 
         # Status report 
         # self.STATUS_URL = ''
         
-        # TOKEN
-        self.STATUS_TOKEN = ''
-        self.STATUS_HOST = ''
-        self.STATUS_PORT = ''
-        self.STATUS_END_POINT = ''
-
-    
-    def writeSecret(self):
-        pass
-
-    def readSecret(self):
-        pass
+        # # TOKEN
+        # self.STATUS_TOKEN = ''
+        # self.STATUS_HOST = ''
+        # self.STATUS_PORT = ''
+        # self.STATUS_END_POINT = ''
